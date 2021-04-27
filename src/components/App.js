fce4b4d5dcf042563459561669d7021c14e1e24b
ahmed-asiri <ia7mad13@gmail.com>
2021-04-27 09:52:19 +0300

    replacing semantic UI with React Bootstrap to use the bootstrap carousel slider

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContentContainer from './ContentContainer';
 import ContentPadding from './ContentPadding';
+import ControlledCarousel from './ControlledCarousel';
 
 import Header from './Header';
 import Navigation from './Navigation';
@@ -12,10 +13,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <ControlledCarousel />
         <div className="layout">
           <Navigation />
           <ContentContainer>
-            
+
           </ContentContainer>
         </div>
       </div>
